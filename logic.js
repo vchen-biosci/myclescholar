332e90b58d1b95fc7c8e1f5a3653b436e44cc0e7
Vivienne Chen <vchen@kellettschool.com>
2026-03-15 22:27:17 +0800

    made font work, added javascript
    I now need to learn css html and javascript for a successful project Lol yay
    i finally got the font to work after an hour... its my first time haha

---
+++ logic.js
@@ -0,0 +1,7 @@
+document.getElementById("welcome").innerHTML = "Welcome to MycleScholar!";
+document.getElementById("tutorial").innerHTML = "This extension helps you track your assessments, work on assignments, and arrange timelines to streamline and romanticise your academic life!";
+const header = document.getElementById("header")
+const headerdraw = header.getContext("2d")
+headerdraw.fillStyle = "Firebrick"
+headerdraw.fillRect(1000,150)
+document.getElementById("monkeycaption").innerHTML = "Here's your mon-KEY companion, 'MYCLE'!";
