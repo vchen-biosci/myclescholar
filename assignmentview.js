04670217f348b0882954a1089e1b82028e1e612f
Vivienne Chen <vchen@kellettschool.com>
2026-03-16 20:14:09 +0800

    final patch
    might scrap this. i found a course on how to do html css and javascript combined so i might study that first before i continue this project. It'll take a few weeks to study so I'll be back. The whole file will likely be reworked after I work on it Lol

---
+++ assignmentview.js
@@ -0,0 +1,4 @@
+document.getElementById('assignment').innerHTML = "This is your assignments page!";
+var assignments_list = pass
+document.getElementById('assignments-list').innerHTML = assignments_list
+document.getElementById('credits').innerHTML = 'Brought to you by MycleBits()! Find us on Instagram and Git!!';
---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('welcome').innerHTML = 'Welcome to MycleScholar! ִֶָ. ..𓂃 ࣪ ִֶָ🪽་༘࿐';
+document.getElementById('tutorial').innerHTML = 'This extension helps you track your assessments, work on assignments, and arrange timelines to streamline and romanticise your academic life!';
+document.getElementById('monkeycaption').innerHTML = "Here's your mon-KEY companion, 'MYCLE'!";
+document.getElementById('credits').innerHTML = 'Brought to you by MycleBits()! Find us on Instagram and Git!!';
+
+function onChange() {
+    let fav_color; {document.getElementById('fav-color').value};
+    document.documentElement.style.setProperty('--primary-color', fav_color);
+}
